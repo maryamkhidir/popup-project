96d7130d6349162fe020203dc1165150958af495
Maryam Khidir <maryamolaide95@gmail.com>
2022-08-03 20:11:41 +0100

    Updates

--- src/store/client.js
+++ src/store/client.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
-/* const BASE_URL = "https://poptin-laravel-api.herokuapp.com/api/" */
-const BASE_URL = "http://127.0.0.1:8000/api/"
+const BASE_URL = "https://poptin-laravel-api.herokuapp.com/api/"
 
 const _client = axios.create({
     baseURL: BASE_URL,
-    timeout: 60 * 1 * 1000, // Let's say you want to wait at least 1 mins
+    timeout: 60 * 1 * 1000, // Let's say we want to wait at least 1 mins
 })
 
 const GET = async (url, data) => 
